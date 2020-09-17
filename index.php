63b33f857aae0cf686c228bcc6260b94c3d2858d
fervi <lordfervi@protonmail.com>
2020-09-17 13:43:26 +0200

    First version of the database added

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 include('config.php');
+include('loader.php');
 include('header.php');
 include('template/navbar.php');
 include('footer.php');
--- loader.php
+++ loader.php
@@ -1,8 +1,11 @@
 <?php
 
-include('config.php');
-
 $xor_cipher = hash('sha512', $xor_password);
+
+// Init database
+if(file_exists($sqlite_database_file)) {
+    $sqlite_db = new SQLite3($sqlite_database_file);
+}
 
 
 ?>
