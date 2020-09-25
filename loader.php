2252fbc5cfeb48bd358926cd6c573c0b907c6990
fervi <lordfervi@protonmail.com>
2020-09-25 15:34:55 +0200

    Added systems to protect session cookies

--- loader.php
+++ loader.php
@@ -7,7 +7,33 @@ if(file_exists($sqlite_database_file)) {
     $sqlite_db = new SQLite3($sqlite_database_file);
 }
 
-session_start();
+// Secure Session
+    header('X-Frame-Options: SAMEORIGIN');
+    session_start();
+
+    if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
+	$secure = true;
+    } else {
+	$secure = false;
+    }
+
+    $httponly = true;
+    $samesite = 'lax';
+
+    if(PHP_VERSION_ID < 70300) {
+        session_set_cookie_params($maxlifetime, '/; samesite='.$samesite, $_SERVER['HTTP_HOST'], $secure, $httponly);
+    } else {
+        session_set_cookie_params([
+            'lifetime' => $maxlifetime,
+            'path' => '/',
+            'domain' => $_SERVER['HTTP_HOST'],
+            'secure' => $secure,
+            'httponly' => $httponly,
+            'samesite' => $samesite
+        ]);
+    }
+
+
 $sqlite_users_db = $sqlite_db;
 $sqlite_games_db = $sqlite_db;
 
--- logout.php
+++ logout.php
@@ -1,5 +1,29 @@
 <?php
 
+	header('X-Frame-Options: SAMEORIGIN');
+
+	if (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
+		$secure = true;
+	} else {
+		$secure = false;
+	}
+
+	$httponly = true;
+	$samesite = 'lax';
+
+	if(PHP_VERSION_ID < 70300) {
+		session_set_cookie_params($maxlifetime, '/; samesite='.$samesite, $_SERVER['HTTP_HOST'], $secure, $httponly);
+	} else {
+		session_set_cookie_params([
+			'lifetime' => $maxlifetime,
+			'path' => '/',
+			'domain' => $_SERVER['HTTP_HOST'],
+			'secure' => $secure,
+			'httponly' => $httponly,
+			'samesite' => $samesite
+		]);
+	}
+
 	session_start();
 	session_destroy();
 	echo '<meta http-equiv="refresh" content="0; url=index.php"/>';
