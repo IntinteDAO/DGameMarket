7a658fad0cf1ba9331171428828486a0ce5dfd61
fervi <lordfervi@protonmail.com>
2020-10-10 21:18:46 +0200

    Added mechanism to send funds to DGameMarket subaccount if transaction is confirmed / rejected.

--- functions/cron/buy_game.php
+++ functions/cron/buy_game.php
@@ -18,6 +18,8 @@ if(php_sapi_name()=="cli") {
 		$id = $db_fetch_invoice['id'];
 		$game_id = $db_fetch_invoice['id_game'];
 		$invoice = $db_fetch_invoice['invoice'];
+		$price = $db_fetch_invoice['price'];
+		$game_fee = $db_fetch_invoice['fee'];
 		$get_key = $sqlite_games_db->querySingle("SELECT key FROM games WHERE id = $game_id", true)['key'];
 		$decode_get_key = decrypt($get_key, $xor_cipher);
 
@@ -38,7 +40,7 @@ if(php_sapi_name()=="cli") {
 
 			// Step 2 - Generate invoice
 			include('functions/payment_providers/'.$payment_provider.'.php');
-			$full_price = $db_fetch_invoice['price'] + $db_fetch_invoice['fee'];
+			$full_price = $price + $game_fee;
 			$buyer = $db_fetch_invoice['buyer'];
 			if($buyer != 0) { $invoice = create_invoice($full_price, "$buyer"); } else { $invoice = create_invoice($full_price, "anonymous"); }
 			$sqlite_invoices_db->query("UPDATE invoices SET invoice = '$invoice' WHERE id = $id");
@@ -67,9 +69,18 @@ if(php_sapi_name()=="cli") {
 				$sqlite_games_db->query("UPDATE games SET status = 3 WHERE (id = $game_id)");
 				$sqlite_games_db->query("UPDATE games SET id_buyer = $buyer WHERE (id = $game_id)");
 				$sqlite_invoices_db->query("UPDATE invoices SET status = 4 WHERE id = $id");
+				$id_seller = $sqlite_games_db->querySingle("SELECT id_seller FROM games WHERE id = $game_id", true)['id_seller'];
+				$sqlite_users_db->query("UPDATE users SET balance = balance + $price WHERE id = $id_seller");
 				echo 'Verification 4 - Updated, game code works.'.PHP_EOL;
 			} else {
-				$sqlite_invoices_db->query("UPDATE invoices SET status = 998 WHERE id = $id");
+
+				if($buyer != 0) {
+					$sqlite_invoices_db->query("UPDATE invoices SET status = 998 WHERE id = $id");
+					$sqlite_users_db->query("UPDATE users SET balance = balance + $price WHERE id = $buyer");
+				} else {
+					$sqlite_invoices_db->query("UPDATE invoices SET status = 997 WHERE id = $id");
+				}
+
 				echo 'Verification 4 - Updated, game code failed, chargeback'.PHP_EOL;
 			}
 
