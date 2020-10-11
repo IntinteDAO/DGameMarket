1a106a59bad1d8696045f06fe89a375ade839294
fervi <lordfervi@protonmail.com>
2020-10-11 17:42:16 +0200

    Optimizing scripts and fixing bugs

--- buy_game.php
+++ buy_game.php
@@ -31,13 +31,12 @@ if(isset($_GET['game_id'])) {
 			$fee = floor(((($fee) / 100000000 / 100) * $bitcoin_price) * 100000000);
 
 			if($buyer != 0) {
-				$is_already_added = $sqlite_invoices_db->querySingle("SELECT id_unique FROM invoices WHERE (id_game = $game_id AND buyer = $buyer AND timestamp < $check_expired_time)", true)['id'];
+				$is_already_added = $sqlite_invoices_db->querySingle("SELECT id_uniq FROM invoices WHERE (id_game = $game_id AND buyer = $buyer AND timestamp < $check_expired_time)", true)['id'];
 			}
 
 			if(empty($is_already_added)) {
-				$sqlite_invoices_db->querySingle("INSERT INTO invoices (id_game, id_unique, buyer, price, fee, invoice, timestamp, status) VALUES ($game_id, '$random_id', $buyer, $price, $fee, 0, 0, 0, $timestamp, 0)", true);
+				$sqlite_invoices_db->querySingle("INSERT INTO invoices (id_game, id_uniq, buyer, price, fee, invoice, timestamp, status) VALUES ($game_id, '$random_id', $buyer, $price, $fee, 0, $timestamp, 0)", true);
 			}
-
 			echo '<meta http-equiv="refresh" content="0; url=?id='.$random_id.'" />';
 		} else {
 			echo 'Wrong key';
@@ -55,17 +54,16 @@ if(isset($_GET['game_id'])) {
 		die('Wrong invoice ID');
 	}
 
-	$is_exists = $sqlite_invoices_db->querySingle("SELECT id_game, buyer, timestamp, id_unique, status, invoice, price, fee FROM invoices WHERE (random_id = '$id')", true);
+	$is_exists = $sqlite_invoices_db->querySingle("SELECT id_game, buyer, timestamp, id_uniq, status, invoice, price, fee FROM invoices WHERE (id_uniq = '$id')", true);
 
 		if(empty($is_exists)) { die("Sorry, this invoice ID is invalid!"); }
 		if($is_exists['buyer'] != $buyer ) { die("Sorry, this invoice is not for you!"); }
 		if((($is_exists['timestamp'] + $expire_buy_time) < $timestamp ) && $is_exists['status']<3) { die("Sorry, this invoice is expired!"); }
 		echo 'Time to expire: '.($is_exists['timestamp'] - $timestamp + $expire_buy_time).'<br>';
-		if($is_exists['status'] == 0) { echo 'Phase 1 / 4 - Verification of the game key.<br>'; }
-		if($is_exists['status'] == 1) { echo 'Phase 2 / 4 - Creating Lightning Invoice.<br>'; }
-		if($is_exists['status'] == 2) {
+		if($is_exists['status'] == 0) { echo 'Phase 1 / 3 - Verification of the game key.<br>'; }
+		if($is_exists['status'] == 1) {
 
-			echo 'Phase 3 / 4 - Waiting for payment.<br>';
+			echo 'Phase 2 / 3 - Waiting for payment.<br>';
 
 			if($qrgenerator == "qrencode") {
 			echo '<img src="functions/qrcode/qrencode.php?text='.$is_exists['invoice'].'">'; }
@@ -74,8 +72,8 @@ if(isset($_GET['game_id'])) {
 			echo $is_exists['price'] + $is_exists['fee'].' sat';
 
 		}
-		if($is_exists['status'] == 3) { echo 'Phase 4 / 4 - Repeat key verification.<br>'; }
-		if($is_exists['status'] == 4) {
+		if($is_exists['status'] == 2) { echo 'Phase 3 / 3 - Repeat key verification.<br>'; }
+		if($is_exists['status'] == 3) {
 
 			echo 'Key ready to release!<br>';
 			$game_id = $is_exists['id_game'];
--- functions/cron/buy_game.php
+++ functions/cron/buy_game.php
@@ -8,7 +8,7 @@ if(php_sapi_name()=="cli") {
 	include('loader.php');
 	include('functions/check_redeem.php');
 	include('functions/encryption.php');
-
+	$provider_initialize = 0;
 
 	$time = time();
 	$db_fetch_invoices = $sqlite_invoices_db->query("SELECT id, id_game, buyer, price, fee, invoice, status FROM invoices WHERE (timestamp + $expire_buy_time) > $time");
@@ -30,6 +30,11 @@ if(php_sapi_name()=="cli") {
 
 			if($verification['value'] == 0) {
 				$sqlite_invoices_db->query("UPDATE invoices SET status = 1 WHERE id = $id");
+				if($provider_initialize == 0) { include('functions/payment_providers/'.$payment_provider.'.php'); $provider_initialize = 1; }
+				$full_price = $price + $game_fee;
+				$buyer = $db_fetch_invoice['buyer'];
+				if($buyer != 0) { $invoice = create_invoice($full_price, "$buyer"); } else { $invoice = create_invoice($full_price, "anonymous"); }
+				$sqlite_invoices_db->query("UPDATE invoices SET invoice = '$invoice' WHERE id = $id");
 				echo 'Verification 1 - Updated, game code works.'.PHP_EOL;
 			} else {
 				$sqlite_invoices_db->query("UPDATE invoices SET status = 999 WHERE id = $id");
@@ -38,37 +43,28 @@ if(php_sapi_name()=="cli") {
 
 		} else if($db_fetch_invoice['status'] == 1) {
 
-			// Step 2 - Generate invoice
-			include('functions/payment_providers/'.$payment_provider.'.php');
-			$full_price = $price + $game_fee;
-			$buyer = $db_fetch_invoice['buyer'];
-			if($buyer != 0) { $invoice = create_invoice($full_price, "$buyer"); } else { $invoice = create_invoice($full_price, "anonymous"); }
-			$sqlite_invoices_db->query("UPDATE invoices SET invoice = '$invoice' WHERE id = $id");
-			$sqlite_invoices_db->query("UPDATE invoices SET status = 2 WHERE id = $id");
-
-		} else if($db_fetch_invoice['status'] == 2) {
-
 			// Step 3 - Verify payment
-			include('functions/payment_providers/'.$payment_provider.'.php');
+			if($provider_initialize == 0) { include('functions/payment_providers/'.$payment_provider.'.php'); $provider_initialize = 1; }
 			$get_all_invoices = get_all_invoices();
 			$count_invoices = count($get_all_invoices)-1;
 			for($i=0; $i<=$count_invoices; $i++) {
 
 				if($get_all_invoices[$i]['pay_req'] == $invoice && !empty($get_all_invoices[$i]['ispaid'])) {
 					// Payment completed
-					$sqlite_invoices_db->query("UPDATE invoices SET status = 3 WHERE id = $id");
+					$sqlite_invoices_db->query("UPDATE invoices SET status = 2 WHERE id = $id");
 				}
 
 			}
-		} else if($db_fetch_invoice['status'] == 3) {
+		} else if($db_fetch_invoice['status'] == 2) {
 
 			// Step 4 - Verify key again
 			$verification = is_redeemed($decode_get_key);
 
 			if($verification['value'] == 0) {
+				$buyer = $db_fetch_invoice['buyer'];
 				$sqlite_games_db->query("UPDATE games SET status = 3 WHERE (id = $game_id)");
-				$sqlite_games_db->query("UPDATE games SET id_buyer = $buyer WHERE (id = $game_id)");
-				$sqlite_invoices_db->query("UPDATE invoices SET status = 4 WHERE id = $id");
+				$sqlite_games_db->query("UPDATE games SET id_buyer = $buyer WHERE id = $game_id");
+				$sqlite_invoices_db->query("UPDATE invoices SET status = 3 WHERE id = $id");
 				$id_seller = $sqlite_games_db->querySingle("SELECT id_seller FROM games WHERE id = $game_id", true)['id_seller'];
 				$sqlite_users_db->query("UPDATE users SET balance = balance + $price WHERE id = $id_seller");
 				echo 'Verification 4 - Updated, game code works.'.PHP_EOL;
